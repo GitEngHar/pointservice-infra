3cc33902f2b31424cb7b87653abf5fc7725c015f
GitEngHar <harukii1999@gmail.com>
2026-03-12 08:42:57 +0900

    update init

--- variables.tf
+++ variables.tf
@@ -1,21 +1,16 @@
-variable "aws_account_id" {
-  description = "AWS Account ID"
-  type        = string
-}
-
-variable "my_global_ip" {
-  description = "MY ACCESS IP"
-  type        = string
-}
-
-variable "line_auth_client_id" {
-  type = string
-}
-
-variable "lambda_exec_role_arn" {
-  type = string
-}
-
-variable "lambda_image_uri" {
-  type = string
-}
+# variable "my_global_ip" {
+#   description = "MY ACCESS IP"
+#   type        = string
+# }
+#
+# variable "line_auth_client_id" {
+#   type = string
+# }
+#
+# variable "lambda_exec_role_arn" {
+#   type = string
+# }
+#
+# variable "lambda_image_uri" {
+#   type = string
+# }
